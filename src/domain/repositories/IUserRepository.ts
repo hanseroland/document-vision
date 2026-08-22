ea9539f8c3798b23e8ba343c94f4ac3f9b07a37c
hanseroland <rolandntougou@gmail.com>
2026-08-22 14:42:09 +0000

    feat: add findByIds, findByActivationToken, findByResetPasswordToken

--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -4,5 +4,8 @@ import { User } from '@domain/entities/User';
 export interface IUserRepository {
   findById(id: string): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
+  findByIds(ids: string[]): Promise<User[]>; 
+  findByActivationToken(token: string): Promise<User | null>;
+  findByResetPasswordToken(token: string): Promise<User | null>;
   save(user:User): Promise<void>;
 }
